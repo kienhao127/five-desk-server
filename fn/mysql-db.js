4fb6e7440b2c1fd1ffc2fac592483da93d90a4f4
haolk <haolk@vng.com.vn>
2018-11-14 01:36:52 +0700

    update db

--- fn/mysql-db.js
+++ fn/mysql-db.js
@@ -3,11 +3,11 @@ var mysql = require('mysql');
 exports.load = function(sql) {
     return new Promise((resolve, reject) => {
         var connection = mysql.createConnection({
-            host: '127.0.0.1',
+            host: 'us-cdbr-iron-east-01.cleardb.net',
             port: '3306',
-            user: 'root',
-            password: '',
-            database: 'website_tuvan'
+            user: 'b375cdf9dcedd3',
+            password: 'ae0a1f00',
+            database: 'heroku_ae446112e19ccfa'
         });
 
         connection.connect();
