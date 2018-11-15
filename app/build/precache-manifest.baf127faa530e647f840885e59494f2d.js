335edced24930ebd56a81b70d20d4423227edf70
haolk <haolk@vng.com.vn>
2018-11-16 02:42:21 +0700

    deploy web

---
+++ app/build/precache-manifest.baf127faa530e647f840885e59494f2d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "001f4f1712e26a721a4f48fd7d98126c",
+    "url": "/static/media/login-image.001f4f17.png"
+  },
+  {
+    "revision": "53826dce3f76969ff327",
+    "url": "/static/js/main.53826dce.chunk.js"
+  },
+  {
+    "revision": "90bb3c7ce3c6a03e40fa3b3b672a0cd9",
+    "url": "/static/media/cover.90bb3c7c.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "676615f5d8f1992a39d68d6dd3097ed5",
+    "url": "/static/media/logo.676615f5.png"
+  },
+  {
+    "revision": "dd4f33f949549a9ffd032f5d7e3018a5",
+    "url": "/static/media/avatar.dd4f33f9.png"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "6c43d21eb497ffbdc9e96e69ee314d17",
+    "url": "/static/media/register-image.6c43d21e.png"
+  },
+  {
+    "revision": "5c62ce8d11d4c8bd639beb6046d4ba3b",
+    "url": "/static/media/Content.5c62ce8d.jpg"
+  },
+  {
+    "revision": "0bcea06c664af39b8857",
+    "url": "/static/js/1.0bcea06c.chunk.js"
+  },
+  {
+    "revision": "0bcea06c664af39b8857",
+    "url": "/static/css/1.73d6471d.chunk.css"
+  },
+  {
+    "revision": "f871af01db682f9b39b0e97d883027f3",
+    "url": "/index.html"
+  }
+];
