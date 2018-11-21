990a1a3cbb72da2d32d7ec93dfb79b60857a0e49
haolk <haolk@vng.com.vn>
2018-11-22 00:36:02 +0700

    sort by UpdateTime

--- repos/mailRepo.js
+++ repos/mailRepo.js
@@ -22,47 +22,47 @@ exports.getMail = function(mailId){
 
 //Chua giai quyet cua ban (StatusId <> 4)
 exports.getNotCloseByUserID = function(mail){
-    var sql = `select * from mail where UserID = '${mail.userID}' and StatusId <> '4' and IsDelete = '0' and CompanyId = '${mail.companyID}' and ReplyTo = ''`;
+    var sql = `select * from mail where UserID = '${mail.userID}' and StatusId <> '4' and IsDelete = '0' and CompanyId = '${mail.companyID}' and ReplyTo = '' order by UpdateTime desc`;
     return db.load(sql);
 }
 
 //Ticket chua chuyen nhuong
 exports.getUnassignedTicket = function(mail){
-    var sql = `select * from mail where UserID IS NULL and IsDelete = '0'  and CompanyId = '${mail.companyID}' and ReplyTo = ''`;
+    var sql = `select * from mail where UserID IS NULL and IsDelete = '0'  and CompanyId = '${mail.companyID}' and ReplyTo = '' order by UpdateTime desc`;
     return db.load(sql);
 }
 
 //Tat ca ticket chua giai quyet (StatusId <> 4)
 exports.getAllNotClose = function(mail){
-    var sql = `select * from mail where StatusId <> '4' and IsDelete = '0' and CompanyId = '${mail.companyID}' and ReplyTo = ''`;
+    var sql = `select * from mail where StatusId <> '4' and IsDelete = '0' and CompanyId = '${mail.companyID}' and ReplyTo = '' order by UpdateTime desc`;
     return db.load(sql);
 }
 
 //Ticket moi (StatusId = 1)
 exports.getNewSticket = function(mail){
-    var sql = `select * from mail where StatusId = '1' and IsDelete = '0' and CompanyId = '${mail.companyID}' and ReplyTo = ''`;
+    var sql = `select * from mail where StatusId = '1' and IsDelete = '0' and CompanyId = '${mail.companyID}' and ReplyTo = '' order by UpdateTime desc`;
     return db.load(sql);
 }
 
 //Ticket cho duyet (StatusId = 3)
 exports.getPendingSticket = function(mail){
-    var sql = `select * from mail where StatusId = '3' and IsDelete = '0' and CompanyId = '${mail.companyID}' and ReplyTo = ''`;
+    var sql = `select * from mail where StatusId = '3' and IsDelete = '0' and CompanyId = '${mail.companyID}' and ReplyTo = '' order by UpdateTime desc`;
     return db.load(sql);
 }
 
 //Ticket da hoan tat (StatusId = 4)
 exports.getClosedSticket = function(mail){
-    var sql = `select * from mail where StatusId = '4' and IsDelete = '0' and CompanyId = '${mail.companyID}' and ReplyTo = ''`;
+    var sql = `select * from mail where StatusId = '4' and IsDelete = '0' and CompanyId = '${mail.companyID}' and ReplyTo = '' order by UpdateTime desc`;
     return db.load(sql);
 }
 
 //Ticket da xoa (StatusId = 4)
 exports.getDeletedSticket = function(mail){
-    var sql = `select * from mail where IsDelete = '1' and CompanyId = '${mail.companyID}' and ReplyTo = ''`;
+    var sql = `select * from mail where IsDelete = '1' and CompanyId = '${mail.companyID}' and ReplyTo = '' order by UpdateTime desc`;
     return db.load(sql);
 }
 
 exports.getMailWithReply = function(mailId){
-    var sql = `select * from mail where MailId = '${mailId}' or ReplyTo = '${mailId}'`;
+    var sql = `select * from mail where MailId = '${mailId}' or ReplyTo = '${mailId}' order by UpdateTime desc`;
     return db.load(sql);
 }
